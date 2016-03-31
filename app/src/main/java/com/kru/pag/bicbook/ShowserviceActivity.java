fb3fb1b11d410f83e79496c52af278adf1a59d80
kittikon <pagsky_2499@hotmail.com>
2016-03-31 02:50:18 -0700

    test11

--- app/src/main/java/com/kru/pag/bicbook/ShowserviceActivity.java
+++ app/src/main/java/com/kru/pag/bicbook/ShowserviceActivity.java
@@ -2,12 +2,36 @@ package com.kru.pag.bicbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ShowserviceActivity extends AppCompatActivity {
 
+
+    //Explicit
+    private TextView showNameTextView;
+    private ListView serviceListView;
+    private String nameString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showservice);
+
+
+
+
+        bindWidget();
+
+
+    }// Main Method
+
+    private void bindWidget() {
+
     }
-}
+
+
+    public void clickReadOrder(View view) {
+
+    }// clickReadOrder
+}// Main Class
